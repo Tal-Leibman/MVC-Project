2d703c0025a74f703d150684e0eb4a0f92e3571c
zim <tral2@walla.com>
2019-05-30 11:09:55 +0300

    Models

---
+++ MVC_Project/Models/Product.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project.Models
+{
+    public class Product
+    {
+        enum States : byte { Available, Reserved, Sold }
+
+        [Required]
+        [HiddenInput(DisplayValue = false)]
+        public long ID { get; set; }
+
+        [Required]
+        [HiddenInput(DisplayValue = false)]
+        public long OwnerID { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public long UserID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Short Description")]
+        public string ShortDescription { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Long Description")]
+        public string LongDescription { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public byte[][] Images { get; set; }
+
+        [Required]
+        [HiddenInput(DisplayValue = false)]
+        public byte State { get; set; }
+    }
+}
---
+++ MVC_Project/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project.Models
+{
+    public class User
+    {
+        [HiddenInput(DisplayValue = false)]
+        public long ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "E-Mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string PasswordHash { get; set; }
+    }
+}
