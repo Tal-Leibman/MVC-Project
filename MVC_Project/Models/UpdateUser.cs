1de9b28910752109603b5c637b6b02389adbcb1c
Tal Leibman <tall@buzzilla.com>
2019-06-16 17:09:05 +0300

    password update works , show relevant errors on fail

---
+++ MVC_Project/Models/UpdateUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Models
+{
+    public class UpdateUser
+    {
+
+        [MaxLength(60)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string CurrentPassword { get; set; }
+
+        [MaxLength(60)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [MaxLength(60)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "First Name")]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Display(Name = "E-Mail")]
+        [MaxLength(254)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+    }
+}
